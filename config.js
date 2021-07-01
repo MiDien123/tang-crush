0c54adef09b736003fc98cc77ade2b22a03e3bb8
MiDien123 <86756587+MiDien123@users.noreply.github.com>
2021-07-01 10:00:27 +0700

    Hi
    Hi

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    introTitle: 'Hi pồ!',
+    introDesc: 'trên thế giới có 7 tỉ đôi môi nhưng em chi cần mỗi đôi môi ank hjhj`,
+    btnIntro: '😍',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: ' ank ju em nhìu nhìu <33',
+    btnNo: 'khum đou tui khum ju em :3',
+    question:'shao ank lại thích em zẫy  <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'tại em đáng ju <33333333',
+    mess: ' em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'hết dịch mình đi chơi nka.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
