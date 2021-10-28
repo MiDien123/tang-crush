9414ad773ec9b4566dbb969244d1217e701253e1
MiDien123 <86756587+MiDien123@users.noreply.github.com>
2021-10-28 08:15:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,17 @@
 const CONFIG = {
-    introTitle: 'Hi pồ!',
-    introDesc: 'trên thế giới có 7 tỉ đôi môi nhưng em chi cần mỗi đôi môi ank hjhj`,
-    btnIntro: '😍',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: ' ank ju em nhìu nhìu <33',
-    btnNo: 'khum đou tui khum ju em :3',
-    question:'shao ank lại thích em zẫy  <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'tại em đáng ju <33333333',
-    mess: ' em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
-    messDesc: 'hết dịch mình đi chơi nka.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+   const CONFIG = {
+    introTitle: 'hi cậu!',
+    introDesc: `tớ khum biết làm thơ chỉ
+    biết rằng tớ ju cậu gất nhìuuu`,
+    btnIntro: 'hihi',
+    title: 'cậu cũng z đúng hok nèeeee😙',
+    desc: 'trả lời đuyyy ',
+    btnYes: 'đúng rùiii',
+    btnNo: 'Khum tui ko thích cậu:3',
+    question:'ụa sao ju tui z ❤',
+    btnReply: 'Gửi cho tớ ❤',
+    reply: ' tại tui đáng ju quó <33',
+    mess: 'ỏ tui bíc gùi nhaaa 😘😘',
+    messDesc: 'z chúng mik ún trà sữa đi.inb liền',
+    btnAccept: 'Okiiiii lun ❤',
+    messLink: 'https://www.facebook.com/settings?tab=account&section=username'www.facebook.com
